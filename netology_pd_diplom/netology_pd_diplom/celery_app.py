a302138eb92a77c181f93dc71fda295de2a04760
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2025-02-11 13:56:20 +0300

    Update celery_app.py

--- netology_pd_diplom/netology_pd_diplom/celery_app.py
+++ netology_pd_diplom/netology_pd_diplom/celery_app.py
@@ -6,3 +6,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netology_pd_diplom.settings')
 app = Celery('netology_pd_diplom')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+__all__ = ['app']
